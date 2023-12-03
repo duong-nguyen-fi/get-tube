cdcfe1bbcd6c4dd3506932855da7ebcfcb9af46e
Duong Nguyen <me@duong-nguyen.com>
2023-12-02 20:41:55 -0500

    Remove audio.wav - change image

--- download-video.sh
+++ download-video.sh
@@ -6,6 +6,6 @@ id=$1
 sudo mkdir -p /files
 sudo chown $(whoami):$(whoami) /files
 rm /files/audio.wav
-#docker run -v downloads:/home/cloud-user/files:Z --rm docker.io/nzuong/get-tube:latest ${id} -x --audio-format wav -P /home/cloud-user/files
+#docker run -v downloads:/home/cloud-user/files:Z --rm tynikel/get-tube:latest ${id} -x --audio-format wav -P /home/cloud-user/files
 docker run  --rm docker.io/nzuong/get-tube:latest ${id} --print filename
-docker run --mount type=bind,source=/files,target=/files --rm docker.io/nzuong/get-tube:latest ${id} -x --audio-format wav -P /files -o audio.wav
+docker run --mount type=bind,source=/files,target=/files --rm tynikel/get-tube:latest ${id} -x --audio-format wav -P /files -o audio.wav
